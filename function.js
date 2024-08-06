1d4e3d1545dc5ff5831c377dfbee758e93fff749
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-08-06 20:12:35 -0300

    g

--- function.js
+++ function.js
@@ -1,3 +1,14 @@
+
 function mostrarAlerta(){
     alert("virus do tigrinho");
+}
+
+//let username = window.prompt("escreva qq coisa");
+//console.log(username);
+let username;
+document.getElementById("mysubmit").onclick =  function pressbutton() {
+username = document.getElementById("input1").value;
+console.log(username)
+document.getElementById("tipo").textContent = ("Hello " + username);
+
 }

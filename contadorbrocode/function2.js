97b6ca02ba1b697457fe25b8b689ea0d3d9cf8f9
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-08-07 10:26:12 -0300

    k

--- contadorbrocode/function2.js
+++ contadorbrocode/function2.js
@@ -1,4 +1,27 @@
-const decreaseBtn = document.getElementById(decreaseBtn);
-const increaseBtn = document.getElementById(increaseBtn);
-const exponentBtn = document.getElementById(exponentBtn);
-const resetBtn = document.getElementById(resetBtn);
+const decreaser = document.getElementById(decreaseBtn);
+const increaser = document.getElementById(increaseBtn);
+const exponent = document.getElementById(exponentBtn);
+const reset = document.getElementById(resetBtn);
+const contador = document.getElementById(simbolocont);
+
+let contagem = 0;
+
+decreaseBtn.onclick = function diminuir() {
+    contagem--;
+    simbolocont.textContent = contagem;
+}
+
+increaseBtn.onclick = function aumentar(){
+    contagem++;
+    simbolocont.textContent = contagem;
+}
+
+exponentBtn.onclick = function expoente(){
+    contagem**
+    simbolocont.textContent = contagem;
+}
+
+resetBtn.onclick = function resetar(){
+    contagem = 0;
+    simbolocont.textContent = contagem;
+}

9571aa567c0231c4d462b2e30889095da2d8c3ba
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-08-07 10:50:16 -0300

    kk

--- contadorbrocode/function2.js
+++ contadorbrocode/function2.js
@@ -3,6 +3,8 @@ const increaser = document.getElementById(increaseBtn);
 const exponent = document.getElementById(exponentBtn);
 const reset = document.getElementById(resetBtn);
 const contador = document.getElementById(simbolocont);
+const round = document.getElementById(roundBtn);
+const divide = document.getElementById(divideBtn);
 
 let contagem = 0;
 
@@ -17,11 +19,25 @@ increaseBtn.onclick = function aumentar(){
 }
 
 exponentBtn.onclick = function expoente(){
-    contagem**
-    simbolocont.textContent = contagem;
+        contagem = Math.pow(contagem, 2); 
+       simbolocont.textContent = contagem;
 }
 
 resetBtn.onclick = function resetar(){
     contagem = 0;
     simbolocont.textContent = contagem;
+}
+
+roundBtn.onclick = function aproximar(){
+    Math.round(contagem);
+    simbolocont.textContent = contagem;
+}
+
+divideBtn.onclick = function dividir(){
+    if (contagem !== 0) { 
+        contagem = contagem / 2;
+    } else {
+        window.alert('Não é possível dividir por zero!');
+    }
+    contador.textContent = contagem;
 }

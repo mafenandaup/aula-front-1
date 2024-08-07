ca8c616e733d69afa44e176eed0cc26f9844741d
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-08-07 10:10:18 -0300

    k

---
+++ contadorbrocode/function2.js
@@ -0,0 +1,4 @@
+const decreaseBtn = document.getElementById(decreaseBtn);
+const increaseBtn = document.getElementById(increaseBtn);
+const exponentBtn = document.getElementById(exponentBtn);
+const resetBtn = document.getElementById(resetBtn);

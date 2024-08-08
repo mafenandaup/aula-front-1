66bde082401ce4400ba9869adc0f06814d0ca6b2
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-08-08 14:28:53 -0300

    kk

---
+++ guessthecolor/functions.js
@@ -0,0 +1,5 @@
+let modofacil = document.getElementById(facilbtn);
+let modomedio = document.getElementById(mediobtn);
+let modofidicil = document.getElementById(dificilbtn);
+
+let cores = ["AMARELO" , "AZUL", "MARROM" , "VERDE" ,"LARANJA" ,"BRANCO", "PRETO", "ROSA", "VERMELHO", "ROXO"]

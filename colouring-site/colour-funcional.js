a41ee637bfdbdf8cecdb05aa421f71062aac00b6
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-10-25 15:35:39 -0300

    kk

--- colouring-site/colour-funcional.js
+++ colouring-site/colour-funcional.js
@@ -1,16 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const music = document.getElementById('bgsong');
-    const muteBtn = document.getElementById('mute-btn');
-
-    if (localStorage.getItem('isMuted') === 'true') {
-        music.muted = true;
-        muteBtn.textContent = 'Unmute';
-    }
-
-    muteBtn.addEventListener('click', () => {
-        music.muted = !music.muted;
-        muteBtn.textContent = music.muted ? 'Unmute' : 'Mute';
-        localStorage.setItem('isMuted', music.muted);
-    });
-});
-
+Document

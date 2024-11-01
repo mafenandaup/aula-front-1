2a6fa0b773d7ad77ba5fcb3a9c96d861eb47ce3e
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-11-01 13:53:04 -0300

    kk

--- aulafront5/functionstest.js
+++ aulafront5/functionstest.js
@@ -1,3 +1,9 @@
 document.getElementById('click-hello').onclick = function() {
     console.log('Olá, Mundo!');
 };
+
+
+let nome = 'Maria';
+const idade = Math.floor(Math.random(20));
+
+console.log('ATIVIDADE 2: Meu nome é' {$nome})

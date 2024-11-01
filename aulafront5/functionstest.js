5a034e85c30bd4a167922690f8689bc8d1003e06
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-11-01 13:56:50 -0300

    kk

--- aulafront5/functionstest.js
+++ aulafront5/functionstest.js
@@ -4,6 +4,5 @@ document.getElementById('click-hello').onclick = function() {
 
 
 let nome = 'Maria';
-const idade = Math.floor(Math.random(20));
-
-console.log('ATIVIDADE 2: Meu nome é' {$nome} 'e eu tenho' {$idade} 'anos.');
+const idade = Math.floor(Math.random() * 30); 
+console.log(`ATIVIDADE 2: Meu nome é ${nome} e eu tenho ${idade} anos.`);

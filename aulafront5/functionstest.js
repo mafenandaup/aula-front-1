7dadefadbaae6f8623581be9aa9cde7bb590f0a7
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-11-01 16:24:15 -0300

    k

--- aulafront5/functionstest.js
+++ aulafront5/functionstest.js
@@ -13,22 +13,17 @@ document.getElementById('submit-ops').addEventListener('click', function() {
 
     if (isNaN(num1) || isNaN(num2)) {
         window.alert('Por favor, insira números válidos.');
-        return;
     }
 
-    // Soma
     const soma = num1 + num2;
     console.log(`Soma: ${num1} + ${num2} = ${soma}`);
 
-    // Subtração
     const subtracao = num1 - num2;
     console.log(`Subtração: ${num1} - ${num2} = ${subtracao}`);
 
-    // Multiplicação
     const multiplicacao = num1 * num2;
     console.log(`Multiplicação: ${num1} * ${num2} = ${multiplicacao}`);
 
-    // Divisão
     if (num2 !== 0) {
         const divisao = num1 / num2;
         console.log(`Divisão: ${num1} / ${num2} = ${divisao}`);

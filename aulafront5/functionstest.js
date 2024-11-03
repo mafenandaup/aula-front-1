9fdac54fbd0cdfbe629241aba33dac8174bd25c0
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-11-03 13:06:38 -0300

    k

--- aulafront5/functionstest.js
+++ aulafront5/functionstest.js
@@ -241,11 +241,10 @@ let carroObj = {
     ano: 1987
 }
 
-document.getElementById('car.btn').addEventListener("click", function(){
-
-console.log('Marca: ' + carro.marca);   
-console.log('Modelo: ' + carro.modelo); 
-console.log('Ano: ' + carro.ano);
- document.getElementById('car-obj-log').textContent(`Marca: ${carro.marca} Modelo: ${carro.modelo}, Ano: ${carro.ano}`);
+document.getElementById('car-btn').addEventListener('click', function() {
+    console.log('Marca: ' + carro.marca);
+    console.log('Modelo: ' + carro.modelo);
+    console.log('Ano: ' + carro.ano);
+    
+    document.getElementById('car-obj-log').textContent = `Marca: ${carro.marca}, Modelo: ${carro.modelo}, Ano: ${carro.ano}`;
 });
-

617253794c8d169bc9f533817ee24f3d9c0517e3
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-11-08 19:49:13 -0300

    kk

--- studiesJS/learnnow.js
+++ studiesJS/learnnow.js
@@ -221,4 +221,17 @@ let temp;
 
 function convert(){
 
+    if(toFahrenheit.checked){
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    }
+    else if(toCelsius.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        result.textContent = "Select a unit"; 
+    }
 }

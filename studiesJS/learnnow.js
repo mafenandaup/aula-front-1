dd3be32d442d75054d41124c56dce825b8a16d5a
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-11-08 15:44:47 -0300

    KKK

--- studiesJS/learnnow.js
+++ studiesJS/learnnow.js
@@ -127,3 +127,8 @@ class Carro {
 
   /// STRICT EQUALITY && STRICT INEQUALITY
   
+//   = assignment operator
+//  == comparison operator (compare if values are equal)
+// === strict equality operator (compare if values & datatype are equal)
+//  != inequality operator
+// !== strict inequality operator

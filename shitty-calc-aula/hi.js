337f4813d11469ec3b63c09fbf1a01d4d891eaf2
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-11-12 07:53:12 -0300

    kk

--- shitty-calc-aula/hi.js
+++ shitty-calc-aula/hi.js
@@ -1,6 +1,7 @@
 
 function subtrair(){
-    
+    const num1 = parseInt(document.getElementById('num1').value)
+    const num2 = parseInt(document.getElementById('num2').value)
 }
 
 function somar(){

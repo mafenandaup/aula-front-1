935967641b13a6bc17230228028d930f1c3e30e9
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-11-14 15:53:42 -0300

    k

--- projeto-front/fortests.js
+++ projeto-front/fortests.js
@@ -38,10 +38,3 @@ document.getElementById('submit-nl').addEventListener('click', function() {
 });
 
         
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-
-hamburger.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
-

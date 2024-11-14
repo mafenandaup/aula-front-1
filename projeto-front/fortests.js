b837d9523883a05b6870bd01a16188e1060ac7a2
Maria Fernanda Maia <mafenandaup@gmail.com>
2024-11-13 22:12:02 -0300

    kk

--- projeto-front/fortests.js
+++ projeto-front/fortests.js
@@ -2,3 +2,20 @@ function toggleMenu() {
   const menu = document.querySelector(".menu");
   menu.classList.toggle("active");  
 }
+
+const nome = document.getElementById('name-nl').value;
+const email = document.getElementById('email-nl').value;
+
+function isValidEmail(useremail, name) {
+  if (useremail.includes("@")) {
+    console.log(`${useremail} is a valid email`);
+    document.getElementById('nl-message').textContent = `Obrigado por se inscrever, ${name} !`
+  } else {
+    console.log(`${useremail} is NOT a valid email`);
+        document.getElementById('nl-message').textContent = 'Por favor, digite um email válido'
+  }
+}
+
+document.getElementById('submit-nl').addEventListener('click', function() {
+  isValidEmail(email, nome); 
+});

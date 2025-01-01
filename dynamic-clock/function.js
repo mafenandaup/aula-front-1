71265424d8834d5e0b57dc0a605b56dc8713c4a5
Maria Fernanda <mafenandaup@gmail.com>
2025-01-01 17:06:54 -0300

    ok

--- dynamic-clock/function.js
+++ dynamic-clock/function.js
@@ -1,3 +1,4 @@
 const hrs = document.getElementById('horas');
 const mnt = document.getElementById('minutos');
 const segs = document.getElementById('segundos');
+let title = document.getElementById('title');

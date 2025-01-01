7818839ebf6ec7d32f3614be0a2f8eda331e6e6d
Maria Fernanda <mafenandaup@gmail.com>
2025-01-01 17:24:58 -0300

    ok

--- dynamic-clock/function.js
+++ dynamic-clock/function.js
@@ -5,10 +5,19 @@ let title = document.getElementById('title');
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
-    let hr =
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = sec;
+
+    if (hr < 10) hr = '0' + hr;
+    if (min< 10) min = '0' + min;
+    if (sec < 10) sec = '0' + sec;
 
 })
-
 
  // Obtém a data atual
  const dataAtual = new Date();

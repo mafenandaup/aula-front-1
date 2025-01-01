3d72ec7a689fa751823e93e8e9d2edcf457bad64
Maria Fernanda <mafenandaup@gmail.com>
2025-01-01 17:19:10 -0300

    ok

--- dynamic-clock/function.js
+++ dynamic-clock/function.js
@@ -1,4 +1,26 @@
-const hrs = document.getElementById('horas');
-const mnt = document.getElementById('minutos');
-const segs = document.getElementById('segundos');
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
 let title = document.getElementById('title');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr =
+
+})
+
+
+ // Obtém a data atual
+ const dataAtual = new Date();
+
+ // Extrai o dia, mês e ano
+ const dia = dataAtual.getDate();
+ const mes = dataAtual.toLocaleString('pt-BR', { month: 'long' }); // Nome do mês
+ const ano = dataAtual.getFullYear();
+ 
+ // Formata a data
+ const dataFormatada = `Hoje é ${dia} de ${mes} de ${ano}!`;
+ 
+ // Insere no elemento HTML
+     document.getElementById('title').textContent = dataFormatada;
+ 
